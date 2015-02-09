eee65d99eb05547a37cc11c08d8613ae57e43924
Ameya Deshpande <ameyade@yahoo.com>
2015-02-08 19:19:49 -0800

    hw section 3 completed

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,10 @@
 require 'erb'
 
+names = Array['Ameya', 'King Ameya', 'King Ameya II', 'King Ameya III']
+
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
-puts template.result(binding)
+template = ERB.new "The value of x is: <%= x + 5 %>"
+template2 = ERB.new "<%= names.each do |t|
+    puts  t 
+end %>"
+puts template2.result(binding)
